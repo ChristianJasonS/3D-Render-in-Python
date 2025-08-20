9ed1b9c5da1d4c19d76ed10690c67bb6418167e0
Christian Jason Sumitro <Christianjasonsumitro010405@gmail.com>
2025-08-19 20:10:02 -0400

    planning tasks

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
 # Itchy Nipples
+
+
+# 1: Create window for pygame.
+    # window resolution, fps tracker, canvas set up
+
+# 2: Create draw() method. Will be called for every event update
+
+# 3: Create run() method. Calls exit() upon window termination.
+
+# 4: Define matrix transformation functions
+    # translation, rotation, scaling
+
+# 5: Create Object class. Should call transformation functions
+
+# 6: Specify horizontal_fov and vertical_fov 
+
